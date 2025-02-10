be9db2b86e8dc58d5047ca0fc7762af1b4d5f689
huyFS <huypbqhe173457@fpt.edu.vn>
2025-02-10 20:35:22 +0700

    HuyDev

--- src/app/[locale]/subBanner/Banner2.tsx
+++ src/app/[locale]/subBanner/Banner2.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { banner2 } from "@/app/data/Banner/banner";
-import { Button } from "@/components/ui/button";
-import { Playwrite_IT_Moderna } from "next/font/google";
-import { useTranslations } from "next-intl";
 
-const playwrite = Playwrite_IT_Moderna({});
 
 const Banner2 = () => {
-    const t = useTranslations("Banner2");
     return (
         <div className="relative w-full h-full">
             <div
--- src/app/[locale]/subBanner/Banner3.tsx
+++ src/app/[locale]/subBanner/Banner3.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { banner3, banner4, banner5 } from "@/app/data/Banner/banner";
-import { Button } from "@/components/ui/button";
-import { Playwrite_IT_Moderna } from "next/font/google";
-import { useTranslations } from "next-intl";
-
-const playwrite = Playwrite_IT_Moderna({});
+import { banner5 } from "@/app/data/Banner/banner";
 
 const Banner3 = () => {
-    const t = useTranslations("Banner3");
     return (
         <div className="relative w-full h-full" >
             <div
