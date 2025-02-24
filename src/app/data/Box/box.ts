fb135ed52fa5b4853da0c0cdeea9cc9d08ace8cb
huyFS <huypbqhe173457@fpt.edu.vn>
2025-02-24 22:40:47 +0700

    HuyDev

--- src/app/data/Box/box.ts
+++ src/app/data/Box/box.ts
@@ -1,3 +1,5 @@
-export const box1: string = "/box/box1.jpg";
-export const box2: string = "/box/box2.webp";
-export const box3: string = "/box/box3.jpg";
+export const box1: string = "/box/C42A9485.JPG";
+export const box2: string = "/box/C42A9496.JPG";
+export const box3: string = "/box/C42A9498.jpg";
+export const boxDetail1: string = "/box/C42A9487.JPG";
+export const boxDetail2: string = "/box/box800.jpg";
--- src/components/data/GiftBox/dataGiftBox.ts
+++ src/components/data/GiftBox/dataGiftBox.ts
@@ -1,32 +1,35 @@
-import { box1, box2, box3 } from "@/app/data/Box/box";
+import { box1, box2, box3 , boxDetail1, boxDetail2 } from "@/app/data/Box/box";
 import { IGiftBox } from "./type";
 
 export const dataGiftBox: IGiftBox[] = [
     {
         id: 1,
         srcImage: box1,
+        srcImagedetail: boxDetail1,
         nameBox: "GiftBox.TetGiftBox.Name",
         detailBox: "GiftBox.TetGiftBox.Detail",
         buttonDetail: "GiftBox.TetGiftBox.Detail Product",
-        price: 500000,
+        price: 350000,
         TransportationMethod: [1, 2],
     },
     {
         id: 2,
-        srcImage: box2,
+        srcImage: boxDetail2,
+        srcImagedetail: boxDetail2,
         nameBox: "GiftBox.LunarNewYearDeluxe.Name",
         detailBox: "GiftBox.LunarNewYearDeluxe.Detail",
         buttonDetail: "GiftBox.TetGiftBox.Detail Product",
-        price: 750000,
+        price: 800000,
         TransportationMethod: [1],
     },
     {
         id: 3,
         srcImage: box3,
+        srcImagedetail: box3,
         nameBox: "GiftBox.SpringFestivalGourmet.Name",
         detailBox: "GiftBox.SpringFestivalGourmet.Detail",
         buttonDetail: "GiftBox.TetGiftBox.Detail Product",
-        price: 850000,
+        price: 400000,
         TransportationMethod: [2],
     },
 ];
